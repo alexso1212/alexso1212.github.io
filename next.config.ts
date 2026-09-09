4d4b14c137a99610ed827a0daa4a48c3d9abea2b
alex <alex@Mac-mini.local>
2026-09-09 19:48:56 +0800

    Deploy site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = { output: 'export' };
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/alex-hello-intro',
+};
 
 export default nextConfig;
